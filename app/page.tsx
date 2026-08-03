535fb2ab8231286977d6888c50ec5cd94ecbd1a2
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2026-08-03 06:23:16 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import LandingPage from "@/components/landing"
 
 export default function Home() {
-  return <LandingPage />
+  return (
+    <>
+      <LandingPage />
+      <script
+        src="https://vm-localization-widget-5.vusercontent.net/widget.js"
+        data-api-key="key_d7af14393b33dfde7526fe23e64dad10cc00354010d570f5b26a20051c8a4e4d"
+      ></script>
+    </>
+  )
 }
-
