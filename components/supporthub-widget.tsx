df947bd0b55ed9638c4baf27385a473f4de4fdaa
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2026-08-03 20:14:14 +0300

    Fix URL format for supporthub-widget script

--- components/supporthub-widget.tsx
+++ components/supporthub-widget.tsx
@@ -15,7 +15,7 @@ export function SupportHubWidget() {
 };`}
       </Script>
       <Script
-        src="https://linksdesk.net/supporthub-widget.js"
+        src="https:/linksdesk.net/supporthub-widget.js"
         data-widget-id="website-main"
         strategy="afterInteractive"
       />
