ef6fb0bdd5f8ccaf318029c54dd25ac7601f5d53
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2026-08-03 06:37:02 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ export default function Home() {
     <>
       <LandingPage />
       <script
-        src="https://vm-localization-widget-5.vusercontent.net/widget.js"
-        data-api-key="key_d7af14393b33dfde7526fe23e64dad10cc00354010d570f5b26a20051c8a4e4d"
-      ></script>
+  src="https://vm-localization-widget-5.vusercontent.net/api/widget/script"
+  data-api-key="key_d7af14393b33dfde7526fe23e64dad10cc00354010d570f5b26a20051c8a4e4d"
+  data-api-url="https://vm-localization-widget-5.vusercontent.net">
+</script>
     </>
   )
 }
